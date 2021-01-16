c9995fa80c8f249aed7b75c25dd4badd64e512cc
Jennifer Kuvlesky <jennifer.kuvlesky@outlook.com>
2021-01-16 07:46:11 -0600

    adding changes with lines

--- Mapping_Lines/Static/js/logic.js
+++ Mapping_Lines/Static/js/logic.js
@@ -2,20 +2,48 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+let map = L.map('mapid').setView([30.18999924, -97.668663992], 5);
 // We create the tile layer that will be the background of our map.
 
-// Coordinates for each point to be used in the line.
-let line = [
+// Coordinates for each point to be used in the line. SFO
+let lineSFOtoATX = [
   [33.9416, -118.4085],
-  [37.6213, -122.3790]
+  [30.18999924, -97.668663992]
 ];
 
-// Coordinates for each point to be used in the line.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790]
-];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(lineSFOtoATX, {
+  color: "blue",
+  dashArray: 7,
+  weight: 4
+}).addTo(map);
+
+// Coordinates for each point to be used in the polyline. SLC
+let lineATXtoTOR = [
+  [30.18999924, -97.668663992],
+  [43.7427, -79.4565]
+  ];
+
+// // Create a polyline using the line coordinates and make the line black.
+L.polyline(lineATXtoTOR, {
+  color: "blue",
+  dashArray: 7,
+  weight: 4
+}).addTo(map);
+
+// // Coordinates for each point to be used in the polyline. SLC
+let lineTORtoJFK = [
+  [43.7427, -79.4565],
+  [40.6413, -73.7781]
+  ];
+
+// // Create a polyline using the line coordinates and make the line black.
+L.polyline(lineTORtoJFK, {
+  color: "blue",
+  dashArray: 7,
+  weight: 4
+}).addTo(map);
+
 // Get data from cities.js
 let cityData = cities;
 
@@ -31,9 +59,10 @@ cityData.forEach(function(city) {
     .addTo(map);
 });
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 15,
+    opacity: 0.5,
     accessToken: API_KEY
     
 });
